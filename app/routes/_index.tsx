61482c47918047440a8be000f20b32567d7896a9
Sam Gaus <sam@gaus.co.uk>
2023-01-27 15:57:02 +0000

    Basic website

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+export default function Index() {
+  return (
+    <div
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        lineHeight: "1.4",
+        textAlign: "center",
+      }}
+    >
+      <h1>Welcome to Collecto</h1>
+      <p>
+        For now, visit <code>/items/&lt;your item id&gt;</code>
+      </p>
+    </div>
+  );
+}
---
+++ app/routes/item.$id.tsx
@@ -0,0 +1,76 @@
+import { prisma } from "~/lib/prisma.server";
+import { json, LoaderArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader({ params }: LoaderArgs) {
+  const id = Number(params.id);
+
+  if (!id) throw json("An item id must be specified", { status: 400 });
+  if (id >= 2 ** 31) throw json("Item not found with that id", { status: 404 });
+
+  const item = await prisma.item.findUnique({
+    where: { id },
+    select: {
+      name: true,
+      picture: true,
+      plural: true,
+      description: true,
+      collection: {
+        select: {
+          quantity: true,
+          player: {
+            select: {
+              name: true,
+              id: true,
+            },
+          },
+        },
+        orderBy: {
+          quantity: "desc",
+        },
+      },
+    },
+  });
+
+  if (!item) throw json("Item not found with that id", { status: 404 });
+
+  return item;
+}
+
+export default function Item() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        lineHeight: "1.4",
+        textAlign: "center",
+      }}
+    >
+      <h2>{data.name}</h2>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          src={`https://s3.amazonaws.com/images.kingdomofloathing.com/itemimages/${data.picture}.gif`}
+        />
+        <p
+          style={{ maxWidth: 700 }}
+          dangerouslySetInnerHTML={{ __html: data.description }}
+        />
+      </div>
+      <ul>
+        {data.collection.map((c, i) => (
+          <li>
+            #{i + 1} {c.player.name} {c.quantity}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
