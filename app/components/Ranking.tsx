23d494382087bc17e6aba799ef11b336df012639
horrible little slime <69secret69email69@gmail.com>
2023-01-28 12:41:04 -0500

    Fix formatting

--- app/components/Ranking.tsx
+++ app/components/Ranking.tsx
@@ -13,43 +13,43 @@ type Props = {
 };
 
 const container: React.CSSProperties = {
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr 1fr",
-    gridTemplateRows: "min-content min-content min-content 1fr",
-    gap: "20px 0px",
-    gridTemplateAreas: `"first first first" "second second second" "third third third" "a b c"`,
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr 1fr",
+  gridTemplateRows: "min-content min-content min-content 1fr",
+  gap: "20px 0px",
+  gridTemplateAreas: `"first first first" "second second second" "third third third" "a b c"`,
 };
 
 export default function Ranking({ collections }: Props) {
-    const [first, second, third, ...rest] = collections;
-    return (
-        <div style={container}>
-            {[
-                { data: first, gridArea: "first", icon: "🥇" },
-                { data: second, gridArea: "second", icon: "🥈" },
-                { data: third, gridArea: "third", icon: "🥉" },
-            ].map(
-                ({ data, gridArea, icon }, i) =>
-                    data && (
-                        <div style={{ gridArea }} key={i}>
-                            {icon} {data.player.name} (
-                            {data.quantity.toLocaleString()})
-                        </div>
-                    )
-            )}
-            {rest.length > 0 &&
-                ["a", "b", "c"].map((gridArea, index) => (
-                    <div style={{ gridArea }} key={index}>
-                        {rest
-                            .filter((_c, i) => i % 3 === index)
-                            .map((c, i) => (
-                                <div key={c.player.id}>
-                                    #{i * 3 + 4 + index} {c.player.name} (
-                                    {c.quantity.toLocaleString()})
-                                </div>
-                            ))}
-                    </div>
-                ))}
-        </div>
-    );
+  const [first, second, third, ...rest] = collections;
+  return (
+    <div style={container}>
+      {[
+        { data: first, gridArea: "first" },
+        { data: second, gridArea: "second" },
+        { data: third, gridArea: "third" },
+      ].map(
+        ({ data, gridArea }, i) =>
+          data && (
+            <div style={{ gridArea }} key={i + 1}>
+              <Rank collection={data} rank={i + 1} />
+            </div>
+          )
+      )}
+      {rest.length > 0 &&
+        ["a", "b", "c"].map((gridArea, index) => (
+          <div style={{ gridArea }} key={index}>
+            {rest
+              .filter((_c, i) => i % 3 === index)
+              .map((c, i) => (
+                <Rank
+                  key={c.player.id}
+                  rank={i * 3 + 4 + index}
+                  collection={c}
+                />
+              ))}
+          </div>
+        ))}
+    </div>
+  );
 }
