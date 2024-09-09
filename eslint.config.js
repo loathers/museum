24f9dac881a0ba0196d42dee4a5ced8517d292ef
Sam Gaus <sam@gaus.co.uk>
2024-09-09 13:39:09 +0100

    Dep upgrades

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,13 @@
-import js from "@eslint/js";
+// @ts-check
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
 
-export default [
-  js.configs.recommended,
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  react.configs.flat["jsx-runtime"],
   {
     ignores: ["public/build", "build"],
-  },
-];
+  }
+);
