ab94d3cda0246de48f701ec8028259a09074f94a
Sam Gaus <sam@gaus.co.uk>
2024-10-23 16:50:19 +0100

    Do not use fragments in an asChild situation

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -24,17 +24,17 @@ export function Button({
     <ChakraButton variant="subtle" disabled={loading || disabled} {...props}>
       {loading ? (
         loadingText ? (
-          <>
+          <span>
             <Spinner size="inherit" color="inherit" />
             {loadingText}
-          </>
+          </span>
         ) : (
-          <>
+          <span>
             <AbsoluteCenter display="inline-flex">
               <Spinner size="inherit" color="inherit" />
             </AbsoluteCenter>
             <Span opacity={0}>{children}</Span>
-          </>
+          </span>
         )
       ) : (
         <HStack>
