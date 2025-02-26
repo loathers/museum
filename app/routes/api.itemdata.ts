39491451fed8851585263a1def209c182567e5f6
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2025-02-26 23:22:19 +0000

    feat: add 'itemdata' API route for mafia use
    Like plural, but contains more things we want to see in items.txt,
    translated to appear more like they do in Mafia.

---
+++ app/routes/api.itemdata.ts
@@ -0,0 +1,33 @@
+import { db } from "~/db.server";
+
+export async function loader() {
+  const items = await db.item.findMany({
+    select: { itemid: true, name: true, picture: true, descid: true, type: true, itemclass: true, candiscard: true, cantransfer: true, quest: true, gift: true, smith: true, cook: true, cocktail: true, jewelry: true, multiuse: true, sellvalue: true, power: true, quest2: true, plural: true },
+    where: { missing: false },
+    orderBy: { itemid: "asc" },
+  });
+
+  return Response.json(
+    items.map((i) => ({
+      id: i.itemid,
+      name: i.name,
+      descid: i.descid,
+      image: i.picture,
+      type: i.type,
+      itemclass: i.itemclass,
+      power: i.power,
+      multiple: i.multiuse,
+      smith: i.smith,
+      cook: i.cook,
+      mix: i.cocktail,
+      jewelry: i.jewelry,
+      d: i.candiscard,
+      t: i.cantransfer,
+      q: i.quest,
+      q2: i.quest2,
+      g: i.gift,
+      autosell: i.sellvalue,
+      plural: i.plural ?? undefined,
+    })),
+  );
+}
