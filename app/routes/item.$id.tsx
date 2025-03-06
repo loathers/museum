d40700165462a8b74ebc81e866a70adb32ee6b85
Sam Gaus <sam@gaus.co.uk>
2025-03-06 12:49:16 +0000

    Add next and prev buttons to item pages

--- app/routes/item.$id.tsx
+++ app/routes/item.$id.tsx
@@ -1,46 +1,58 @@
-import { Button, Group, HStack, Heading, Image, Stack } from "@chakra-ui/react";
-import { LuArrowLeft } from "react-icons/lu";
-import {
-  type LoaderFunctionArgs,
-  type MetaFunction,
-  Link as RRLink,
-  useLoaderData,
-} from "react-router";
+import { Button, Heading, IconButton, Image, Stack } from "@chakra-ui/react";
+import { LuArrowLeft, LuArrowRight, LuHouse } from "react-icons/lu";
+import { Link as RRLink, useLoaderData } from "react-router";
 
+import { Route } from "./+types/api.item.$id";
 import ItemDescription from "~/components/ItemDescription";
 import ItemPageRanking from "~/components/ItemPageRanking";
 import Layout from "~/components/Layout";
+import { db } from "~/db.server";
 import { itemToString } from "~/utils";
 import { HttpError, type SlimItem, loadCollections } from "~/utils.server";
 
-export const loader = async ({ params }: LoaderFunctionArgs) => {
+export const loader = async ({ params }: Route.LoaderArgs) => {
   const id = Number(params.id);
+  console.log(params, id);
   try {
-    return await loadCollections(id);
+    return {
+      item: await loadCollections(id),
+      prev: await db.item.findFirst({
+        where: { itemid: { lt: id }, seen: { isNot: null } },
+        orderBy: { itemid: "desc" },
+      }),
+      next: await db.item.findFirst({
+        where: { itemid: { gt: id }, seen: { isNot: null } },
+        orderBy: { itemid: "asc" },
+      }),
+    };
   } catch (error) {
     if (error instanceof HttpError) throw error.toRouteError();
     throw error;
   }
 };
 
-export const meta: MetaFunction<typeof loader> = ({ data: item }) => [
+export const meta = ({ data: { item } }: Route.MetaArgs) => [
   { title: `Museum :: ${itemToString(item)}` },
 ];
 
 function ItemLayout({
   children,
   item,
+  prev,
+  next,
   wiki,
 }: React.PropsWithChildren<{
   item: SlimItem & { picture: string };
+  prev: SlimItem | null;
+  next: SlimItem | null;
   wiki: boolean;
 }>) {
   const wikiLink = `https://kol.coldfront.net/thekolwiki/index.php/${itemToString(item)}`;
 
   return (
     <Layout>
-      <Stack>
-        <HStack gap={6} justifyContent="center">
+      <Stack alignSelf="stretch">
+        <Stack direction="row" gap={6} justifyContent="center">
           <Image
             src={`https://s3.amazonaws.com/images.kingdomofloathing.com/itemimages/${item.picture}.gif`}
             alt={itemToString(item)}
@@ -52,27 +64,50 @@ function ItemLayout({
             size="4xl"
             dangerouslySetInnerHTML={{ __html: item.name }}
           />
-        </HStack>
-        <Group justifyContent="center">
-          <Button asChild>
-            <RRLink to="/">
+        </Stack>
+        <Stack direction="row" justifyContent="space-between">
+          <IconButton disabled={!prev} asChild>
+            <RRLink
+              to={prev ? `/item/${prev.itemid}` : "."}
+              title={
+                prev
+                  ? `Previous item (${itemToString(prev)})`
+                  : "No previous item"
+              }
+            >
               <LuArrowLeft />
-              home
             </RRLink>
-          </Button>
-          {wiki && (
+          </IconButton>
+          <Stack direction="row">
             <Button asChild>
-              <RRLink to={wikiLink} target="_blank" rel="noopener noreferrer">
-                <img
-                  src="/coldfront.png"
-                  alt="Wiki link"
-                  style={{ width: "1em", verticalAlign: "middle" }}
-                />
-                wiki
+              <RRLink to="/">
+                <LuHouse />
+                home
               </RRLink>
             </Button>
-          )}
-        </Group>
+            {wiki && (
+              <Button asChild>
+                <RRLink to={wikiLink} target="_blank" rel="noopener noreferrer">
+                  <img
+                    src="/coldfront.png"
+                    alt="Wiki link"
+                    style={{ width: "1em", verticalAlign: "middle" }}
+                  />
+                  wiki
+                </RRLink>
+              </Button>
+            )}
+          </Stack>
+          <IconButton
+            disabled={!next}
+            asChild
+            title={next ? `Next item (${itemToString(next)})` : "No next item"}
+          >
+            <RRLink to={next ? `/item/${next.itemid}` : "."}>
+              <LuArrowRight />
+            </RRLink>
+          </IconButton>
+        </Stack>
       </Stack>
 
       {children}
@@ -81,33 +116,12 @@ function ItemLayout({
 }
 
 export default function Item() {
-  const item = useLoaderData<typeof loader>();
+  const { item, prev, next } = useLoaderData<typeof loader>();
 
   return (
-    <ItemLayout wiki={true} item={item}>
+    <ItemLayout wiki={true} item={item} prev={prev} next={next}>
       <ItemDescription description={item.description} />
       <ItemPageRanking collections={item.collections} />
     </ItemLayout>
   );
 }
-
-// export function ErrorBoundary() {
-//   const error = useRouteError();
-
-//   console.log(error);
-
-//   if (!isRouteErrorResponse(error)) return "Unexpected error";
-
-//   const item = {
-//     itemid: -1,
-//     name: "Not Found",
-//     picture: "nopic",
-//     ambiguous: false,
-//   };
-
-//   return (
-//     <ItemLayout item={item} wiki={false}>
-//       <Text>{error.data}</Text>
-//     </ItemLayout>
-//   );
-// }
