aaee3962016fac5daaf4796e60f8b6439d9fdeff
Sam Gaus <sam@gaus.co.uk>
2026-05-10 21:07:42 +0100

    Use join in api.player route to reduce query count

--- app/routes/api.player.$id.ts
+++ app/routes/api.player.$id.ts
@@ -5,35 +5,39 @@ import { HttpError } from "~/utils.server";
 export async function loader({ params }: Route.LoaderArgs) {
   const id = Number(params.id);
 
-  const player = await db
-    .selectFrom("Player")
-    .select(["playerid", "name"])
-    .where("playerid", "=", id)
-    .executeTakeFirst();
+  const [rows, nameChanges] = await Promise.all([
+    db
+      .selectFrom("Player")
+      .leftJoin("Collection", "Collection.playerid", "Player.playerid")
+      .select([
+        "Player.playerid",
+        "Player.name",
+        "Collection.quantity",
+        "Collection.rank",
+        "Collection.itemid",
+      ])
+      .where("Player.playerid", "=", id)
+      .orderBy("Collection.itemid", "asc")
+      .execute(),
+    db
+      .selectFrom("PlayerNameChange")
+      .select(["oldname", "when"])
+      .where("playerid", "=", id)
+      .orderBy("when", "desc")
+      .execute(),
+  ]);
 
-  if (!player)
+  if (rows.length === 0)
     return Response.json(
       new HttpError(404, "Player not found with that id").toRouteError(),
       { status: 404 },
     );
 
-  const collections = await db
-    .selectFrom("Collection")
-    .select(["quantity", "rank", "itemid"])
-    .where("playerid", "=", id)
-    .orderBy("itemid", "asc")
-    .execute();
+  const { playerid, name } = rows[0];
+  const collections =
+    rows[0].itemid === null
+      ? []
+      : rows.map(({ quantity, rank, itemid }) => ({ quantity, rank, itemid }));
 
-  const nameChanges = await db
-    .selectFrom("PlayerNameChange")
-    .select(["oldname", "when"])
-    .where("playerid", "=", id)
-    .orderBy("when", "desc")
-    .execute();
-
-  return Response.json({
-    ...player,
-    collections,
-    nameChanges,
-  });
+  return Response.json({ playerid, name, collections, nameChanges });
 }
